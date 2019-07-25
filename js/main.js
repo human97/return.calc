a68b2c7f8cb498bdc152eb4a634cd85e3bd30045
human97 <aleksey.n.popov@gmail.com>
2019-07-26 02:53:53 +0400

    optimisation js code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let outsum = document.querySelector('#outsum');
-rangeSum = document.querySelector('#rangeSum');
+let rangeSum = document.querySelector('#rangeSum'); //сумма вклада
 rangeSum.oninput = function () {
 	outsum.value = rangeSum.value;
 }
 
 let outMonth = document.querySelector('#outMonth');
-rangeMonth = document.querySelector('#rangeMonth');
+let rangeMonth = document.querySelector('#rangeMonth');
 rangeMonth.oninput = function () {
 	outMonth.value = rangeMonth.value;
 }
@@ -13,32 +13,23 @@ rangeMonth.oninput = function () {
 // Формула для вкладов с ежемесячной капитализацией
 // Доходность=Сумма*(1+(Год.проц./12))**Месяцев
 let rp = 0.065; // фиксированная годовая проц.ставка для депозита/100
-let rpd = rp / 12; // проц.ставка в месяц
-//let rpcR=rpc/12; // проц.ставка в месяц из radio
-let rpdMonth = 1 + rpd;
-let returnDeposit = document.querySelector('#return__deposit');
-let returnCityMoney = document.querySelector('#return__cityMoney');
+let returnDeposit = document.querySelector('#return__deposit'); //вывод доходности депозита под 6.5%
+let returnCityMoney = document.querySelector('#return__cityMoney'); //вывод доходности города денег согласно тарифу
 
 function count() { // функция расчета доходности для вкладов с ежемесячной капитализацией
-	rdpY = rpdMonth ** rangeMonth.value;
-	returnDep = rangeSum.value * rpdMonth * rdpY;
-	returnDep = returnDep - rangeSum.value;
+	let returnDep = (rangeSum.value * ((1 + rp / 12) ** rangeMonth.value)) - rangeSum.value; //доходность депозита под 6.5%
 	returnDeposit.value = returnDep.toFixed();
-	rpcRadio = document.querySelectorAll('.percent');
-	let rpValue; //годовая %ставка из radio для формулы
+
+	let rpcRadio = document.querySelectorAll('.percent'); // выбор тарифа
 	for (let i = 0; i < rpcRadio.length; i++) {
 		if (rpcRadio[i].checked) {
-			rpValue = rpcRadio[i].value;
+			rpValue = rpcRadio[i].value; //годовая %ставка из radio для формулы
 			let rp100 = rpValue * 100; //годовая %ставка из radio
 			document.querySelector('#percent__right').textContent = rp100 + '% годовых';
 		}
 	}
 
-	let rpc = rpValue / 12;
-	let rpcMonth = 1 + rpc;
-	let rdcY = rpcMonth ** rangeMonth.value;
-	returnCity = rangeSum.value * rpcMonth * rdcY;
-	returnCity = returnCity - rangeSum.value;
+	returnCity = (rangeSum.value * ((1 + rpValue / 12) ** rangeMonth.value)) - rangeSum.value;
 	returnCityMoney.value = returnCity.toFixed();
 }
 
